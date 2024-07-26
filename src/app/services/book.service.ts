d7b06e1c3e6f498234d1d5d57730d8e965f605a8
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2024-07-26 12:18:17 -0600

    added in progress books, moved thrawn ascendancy 2, changed amazon and gps links to ids

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -19,22 +19,12 @@ export class BookService {
   public getInProgressBooks(): Book[] {
     return [
       {
-        id: 19,
-        name: "Thrawn Ascendancy: Greater Good",
-        fileName: "greater good.jpg",
-        appleLink: "",
-        amazonLink: "https://smile.amazon.com/dp/1529101921",
-        googlePlayLink: "https://play.google.com/store/books/details?id=OdMAEAAAQBAJ",
-        pros: "As usual, the space battles are amazing. Thrawn's tactical analysis of war and his enemy is as good as ever here, and that alone makes this a must read. Each winding path and story told in the book concludes in a very satisfying way. How this book fits into the overall timeline is also very satisfying, and the ending of this book saved it greatly for me. I think my favorite thing to come of this new Thrawn trilogy is the world building and lore around the Chiss families. This was something I grew to love and understand, and the breadth of political and non-political decision making based on family was very well done.",
-        cons: "This book focuses less on Thrawn and more on the overall situation that the Chaos is in, which isn't a bad thing. The secondary characters are still very interesting. This book does feel like Thrawn is making a lot of guesses rather than decisions based on certainty and analysis, as he would in other books. Even though I loved the family politics in this book, it was difficult to keep up with everyone's allegiances, something I'm sure would be better with a second reading."
-      },
-      {
         id: 20,
         name: "The Rising Storm",
         fileName: "The Rising Storm.jpg",
         appleLink: "",
-        amazonLink: "https://smile.amazon.com/dp/0593159411",
-        googlePlayLink: "https://play.google.com/store/books/details?id=hmQBEAAAQBAJ",
+        amazonLink: "0593159411",
+        googlePlayLink: "hmQBEAAAQBAJ",
         pros: "",
         cons: ""
       },
@@ -43,18 +33,8 @@ export class BookService {
         name: "Alphabet Squadron: Shadow Fall",
         fileName: "shadow fall.jpg",
         appleLink: "",
-        amazonLink: "https://www.amazon.com/dp/1984820044",
-        googlePlayLink: "https://play.google.com/store/books/details?id=58WyDwAAQBAJ",
-        pros: "",
-        cons: ""
-      },
-      {
-        id: 21,
-        name: "Phasma",
-        fileName: "phasma.jpg",
-        appleLink: "",
-        amazonLink: "https://smile.amazon.com/dp/1524796336",
-        googlePlayLink: "https://play.google.com/store/books/details?id=48WtDgAAQBAJ",
+        amazonLink: "1984820044",
+        googlePlayLink: "58WyDwAAQBAJ",
         pros: "",
         cons: ""
       },
@@ -63,8 +43,8 @@ export class BookService {
         name: "Thrawn Ascendancy: Lesser Evil",
         fileName: "lesser evil.jpg",
         appleLink: "",
-        amazonLink: "https://www.amazon.com/dp/0593158326",
-        googlePlayLink: "https://play.google.com/store/books/details?id=bWIkEAAAQBAJ",
+        amazonLink: "0593158326",
+        googlePlayLink: "bWIkEAAAQBAJ",
         pros: "",
         cons: ""
       },
@@ -73,8 +53,8 @@ export class BookService {
         name: "Battlefront II: Inferno Squad",
         fileName: "inferno squad.jpg",
         appleLink: "",
-        amazonLink: "https://amazon.com/dp/1524796808",
-        googlePlayLink: "https://play.google.com/store/books/details?id=1QYEDgAAQBAJ",
+        amazonLink: "1524796808",
+        googlePlayLink: "1QYEDgAAQBAJ",
         pros: "",
         cons: ""
       },
@@ -83,8 +63,8 @@ export class BookService {
         name: "Dark Disciple",
         fileName: "dark disciple.jpg",
         appleLink: "",
-        amazonLink: "https://amazon.com/dp/0345511530",
-        googlePlayLink: "https://play.google.com/store/books/details?id=qSdsBQAAQBAJ",
+        amazonLink: "0345511530",
+        googlePlayLink: "qSdsBQAAQBAJ",
         pros: "",
         cons: ""
       },
@@ -93,11 +73,111 @@ export class BookService {
         name: "Queen's Peril",
         fileName: "queens peril.jpg",
         appleLink: "",
-        amazonLink: "https://amazon.com/dp/1368057144",
-        googlePlayLink: "https://play.google.com/store/books/details?id=M5XUDwAAQBAJ",
+        amazonLink: "1368057144",
+        googlePlayLink: "M5XUDwAAQBAJ",
         pros: "",
         cons: ""
       },
+      {
+        id: 26,
+        name: "Jedi: Battle Scars",
+        fileName: "jedi battle scars.jpg",
+        appleLink: "",
+        amazonLink: "0593598601",
+        googlePlayLink: "eOZzEAAAQBAJ",
+        pros: "",
+        cons: ""
+      },
+      {
+        id: 27,
+        name: "Bloodline",
+        fileName: "bloodline.jpg",
+        appleLink: "",
+        amazonLink: "0425286789",
+        googlePlayLink: "IaXSCQAAQBAJ",
+        pros: "",
+        cons: ""
+      },
+      {
+        id: 28,
+        name: "Midnight Horizons",
+        fileName: "Midnight Horizons.jpg",
+        appleLink: "",
+        amazonLink: "1368060676",
+        googlePlayLink: "BjdVEAAAQBAJ",
+        pros: "",
+        cons: ""
+      },
+      {
+        id: 29,
+        name: "Shadow of the Sith",
+        fileName: "Shadow of the Sith.jpg",
+        appleLink: "",
+        amazonLink: "0593358600",
+        googlePlayLink: "BNBFEAAAQBAJ",
+        pros: "",
+        cons: ""
+      },
+      {
+        id: 30,
+        name: "Brotherhood",
+        fileName: "Brotherhood.jpg",
+        appleLink: "",
+        amazonLink: "0593358570",
+        googlePlayLink: "MDY_EAAAQBAJ",
+        pros: "",
+        cons: ""
+      },
+      {
+        id: 31,
+        name: "Rogue One: A Star Wars Story",
+        fileName: "Rogue One A Star Wars Story.jpg",
+        appleLink: "",
+        amazonLink: "0399178473",
+        googlePlayLink: "25n9CwAAQBAJ",
+        pros: "",
+        cons: ""
+      },
+      {
+        id: 32,
+        name: "The Fallen Star",
+        fileName: "The Fallen Star.jpg",
+        appleLink: "",
+        amazonLink: "0593355393",
+        googlePlayLink: "hV45EAAAQBAJ",
+        pros: "",
+        cons: ""
+      },
+      {
+        id: 33,
+        name: "Tempest Runner",
+        fileName: "Tempest Runner.jpg",
+        appleLink: "",
+        amazonLink: "0593358996",
+        googlePlayLink: "Neg6EAAAQBAJ",
+        pros: "",
+        cons: ""
+      },
+      {
+        id: 34,
+        name: "Path of Deceit",
+        fileName: "Path of Deceit.jpg",
+        appleLink: "",
+        amazonLink: "1368076122",
+        googlePlayLink: "KX56EAAAQBAJ",
+        pros: "",
+        cons: ""
+      },
+      {
+        id: 35,
+        name: "The Battle of Jedha",
+        fileName: "The Battle of Jedha.jpg",
+        appleLink: "",
+        amazonLink: "0593597893",
+        googlePlayLink: "6MpvEAAAQBAJ",
+        pros: "",
+        cons: ""
+      }
     ];
   }
 
